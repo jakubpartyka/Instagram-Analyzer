e34028ccc4406fde9054c654d1067b19ed098fea
jakubpartyka <j.partyka20@gmail.com>
2020-01-28 04:01:30 +0100

    additional waits added in Report.generate() method, failCounter implemendet and additional scrolling added for better profile loadinf

--- src/Report.java
+++ src/Report.java
@@ -6,15 +6,19 @@ import java.awt.event.InputEvent;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 class Report {
+    List<Profile> followers = new ArrayList<>();
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("DD.MM.YYYY HH:mm");
     private int mouseWheelValue = -30;
     private Profile target;
-    Date date;
     private String status;
+    Date date;
 
     Report(Profile target) {
         this.target = target;
@@ -26,6 +30,12 @@ class Report {
         driver.get(target.getLink());
 
         WebElement followers = driver.findElements(By.xpath("//*[@class='g47SY ']")).get(1);
+        int expectedFollowersSize = 0;
+        try {
+            expectedFollowersSize = Integer.parseInt(followers.getText());
+        }
+        catch (Exception ignored){}
+
         int followersCount = Integer.parseInt(followers.getText());
         if(followersCount > 5000){
             log("skipping profile because follower count is over 5000: " + target);
@@ -54,11 +64,13 @@ class Report {
             }
         }
 
-        Robot robot;
+
         //focus on popup window
+        Robot robot;
         try {
-            Point cords = firstProfile.getLocation();
             robot = new Robot();
+            Point cords = firstProfile.getLocation();
+            //moving to right side of the first profile in popup window to focus
             robot.mouseMove(cords.getX() + 200, cords.getY() + 300);
             robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
             robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
@@ -73,7 +85,9 @@ class Report {
         Thread.sleep(100);
 
         List<WebElement> profiles = driver.findElements(By.xpath("//*[@class='FPmhX notranslate  _0imsa ']"));
-        int initialSize;
+        int initialSize,
+                failCounter = 0;    //this counts how many times number of loaded profiles was incorrect (different from expected by 10 or more)
+        log("loading profiles");
         do {
             //in case of problems at this point try changing mouseWheelValue to +30 (positive) instead of -30
             //! - javaScriptExecutors does not work here! Robot class has to be used instead.
@@ -89,14 +103,45 @@ class Report {
             profiles = driver.findElements(By.xpath("//*[@class='FPmhX notranslate  _0imsa ']"));
             if(profiles.size() == initialSize){
                 Thread.sleep(200);
+                robot.mouseWheel(mouseWheelValue);
+                failCounter++;
                 profiles = driver.findElements(By.xpath("//*[@class='FPmhX notranslate  _0imsa ']"));
                 if(profiles.size() == initialSize){
                     //wait for the rest page to load
-                    Thread.sleep(500);
+                    Thread.sleep(400);
+                    robot.mouseWheel(mouseWheelValue);
+                    failCounter++;
                     profiles = driver.findElements(By.xpath("//*[@class='FPmhX notranslate  _0imsa ']"));
+                    if(expectedFollowersSize - followersCount > 5){
+                        Thread.sleep(500);      //additional wait based on expected profile count
+                        robot.mouseWheel(mouseWheelValue);
+                        failCounter++;
+                    }
                 }
             }
-        } while (profiles.size() > initialSize);        //scrolling down until new profiles appear
+            if(failCounter == 10) {
+                log("breaking loop because of fail count");
+                break;              //if 10 fails occurs there's a different problem and loop should be exited
+            }
+        } while (profiles.size() > initialSize || expectedFollowersSize - profiles.size() < 10);        //scrolling down as long as new profiles appear
+        log("loading profiles finished. Loaded profiles: " + profiles.size());
+
+        profiles.forEach(profile -> {
+            String link = profile.getAttribute("href");
+            //name location could be handled here
+            this.followers.add(new Profile(link));      //adding to report list
+            if(Main.isLogToConsole())
+                System.out.println("added: " + link);
+        });
+        log("listing followers complete. Number of followers listed: " + this.followers.size());
+
+        setDateNow();   //set the date of report. This should be the last instruction in this method
+        log("generating report completed");
+    }
+
+    private void setDateNow() {
+        this.date = Calendar.getInstance().getTime();
+        log("date set to " + dateFormat.format(date));
     }
 
     private static void log(String message) {
